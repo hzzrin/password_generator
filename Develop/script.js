57881a86c21761eb373d31bc1d29b4a98e0f0fc7
Hyerin Kim <hr9130@gmail.com>
2021-03-15 14:50:03 +1100

    V2 restructured the script

--- Develop/script.js
+++ Develop/script.js
@@ -15,54 +15,22 @@ function writePassword() {
 }
 
 function generatePassword() {
-    var charLength = 0
+    var charLength = Number(prompt("How many characters would you like your password to be? (8-128)"));
     while ((charLength < 8 || charLength > 128) || Number.isInteger(charLength) === false) {
-        charLength = parseInt(prompt("How many characters would you like your password to be? (8-128)"));
+        charLength = Number(prompt("Length must be 8-128 characters!"));
     }
 
-    var upperCase = false
-    var lowerCase = false
-    var numericalCase = false
-    var specialCase = false
-
-    while (!upperCase && !lowerCase && !numericalCase && !specialCase) {
-
-        upperCase = confirm("Click OK if you would like to include uppercase characters");
-        lowerCase = confirm("Click OK if you would like to include lowercase characters");
-        numericalCase = confirm("Click OK if you would like to include numeric characters");
-        specialCase = confirm("Click OK if you would like to include special characters (~!@#$%^&*()-_=+)");
+    var upperCase = confirm("Click OK if you would like to include uppercase characters");
+    var lowerCase = confirm("Click OK if you would like to include lowercase characters");
+    var numberCase = confirm("Click OK if you would like to include numeric characters");
+    var specialCase = confirm("Click OK if you would like to include special characters (~!@#$%^&*()-_=+)");
+    while (!upperCase && !lowerCase && !numberCase && !specialCase) {
+      alert("You must select at least one character type!");
+      upperCase = confirm("Click OK if you would like to include uppercase characters");
+      lowerCase = confirm("Click OK if you would like to include lowercase characters");
+      numeberCase = confirm("Click OK if you would like to include numeric characters");
+      specialCase = confirm("Click OK if you would like to include special characters (~!@#$%^&*()-_=+)");
     }
-
-    if (upperCase) {
-        userInput.push(charRange[0]);
-    }
-
-    if (lowerCase) {
-        userInput.push(charRange[1])
-    }
-
-    if (numericalCase) {
-        userInput.push(charRange[2])
-    }
-
-    if (specialCase) {
-        userInput.push(charRange[3])
-    }
-
-    var password = "";
-
-    userInput = userInput.join("").split("");
-
-
-    for (var i = 0; i < charLength; i++) {
-
-        var index = (Math.floor(Math.random() * userInput.length));
-        password = password + userInput[index]
-    }
-
-    return password
-
 }
-
 
 generateBtn.addEventListener("click", writePassword);
